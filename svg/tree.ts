631d19e412aac8ff3aa5bc0f94381fef54b9e79e
Will Kim <will@chequer.io>
2022-02-10 15:57:41 +0900

    t Change dir name

---
+++ svg/tree.ts
@@ -0,0 +1,96 @@
+// === made by querypie front-end team
+
+const tree = {
+	"path": "svg",
+	"name": "svg",
+	"children": [
+		{
+			"path": "svg/arrows",
+			"name": "arrows",
+			"children": [
+				{
+					"path": "svg/arrows/ArrowDown.svg",
+					"name": "ArrowDown.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/arrows/ArrowDownSmall.svg",
+					"name": "ArrowDownSmall.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/arrows/ArrowLeft.svg",
+					"name": "ArrowLeft.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/arrows/ArrowRight.svg",
+					"name": "ArrowRight.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/arrows/ArrowUp.svg",
+					"name": "ArrowUp.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/arrows/ArrowUpSmall.svg",
+					"name": "ArrowUpSmall.svg",
+					"type": "file"
+				}
+			],
+			"type": "directory"
+		},
+		{
+			"path": "svg/base",
+			"name": "base",
+			"children": [
+				{
+					"path": "svg/base/ArrowDown.svg",
+					"name": "ArrowDown.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/base/ArrowDownSmall.svg",
+					"name": "ArrowDownSmall.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/base/ArrowLeft.svg",
+					"name": "ArrowLeft.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/base/ArrowRight.svg",
+					"name": "ArrowRight.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/base/ArrowUp.svg",
+					"name": "ArrowUp.svg",
+					"type": "file"
+				},
+				{
+					"path": "svg/base/ArrowUpSmall.svg",
+					"name": "ArrowUpSmall.svg",
+					"type": "file"
+				}
+			],
+			"type": "directory"
+		},
+		{
+			"path": "svg/temp",
+			"name": "temp",
+			"children": [],
+			"type": "directory"
+		},
+		{
+			"path": "svg/temp.svg",
+			"name": "temp.svg",
+			"type": "file"
+		}
+	],
+	"type": "directory"
+};
+
+export default tree;
