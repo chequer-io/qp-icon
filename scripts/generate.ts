2d9d8bc63da153917ffb8a835e68f937786ac7ee
Will Kim <will@chequer.io>
2022-02-17 03:31:36 +0900

    Add story build func & Change test svg files

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,11 +1,12 @@
 import { existsSync } from 'fs';
 import { writeFile } from 'fs/promises';
 import dirTree from 'directory-tree';
-import { execSync } from "child_process";
+import { execSync } from 'child_process';
 
 // relative to package.json
 export const SVG_DIR = 'svg';
 export const COMPONENT_DIR = 'components';
+export const STORY_DIR = 'stories';
 export const TREE_FILENAME = `tree.ts`;
 
 const checkDir = async (dirPath: string) => {
@@ -14,7 +15,7 @@ const checkDir = async (dirPath: string) => {
   } catch (error) {
     throw new Error(`directory [${dirPath}] is not exist`);
   }
-}
+};
 
 const makeFile = async (filename: string, body: string) => {
   const data = new Uint8Array(Buffer.from(body));
@@ -26,13 +27,13 @@ const makeFile = async (filename: string, body: string) => {
   }
 };
 
-const getTreeFileBody = (jsonTree: string) => `
+const getTreeFileBody = (jsonTree: string) =>
+  `
 // === made by querypie front-end team
-
 const tree = ${jsonTree};
 
 export default tree;
-`.trim() + '\r\n'
+`.trim() + '\r\n';
 
 const generate = async () => {
   console.log(`🔍 Checking [${SVG_DIR}/] exists ...`);
@@ -47,8 +48,9 @@ const generate = async () => {
   await makeFile(`${SVG_DIR}/${TREE_FILENAME}`, getTreeFileBody(svgJsonTree));
 
   console.log(`🚚 Extracting react components from svg files ...`);
-  const TEMPLATE_PATH_EXCEPT_EXT = './scripts/svgrTemplate'
-  execSync(`
+  const TEMPLATE_PATH_EXCEPT_EXT = './scripts/svgrTemplate';
+  execSync(
+    `
 tsc ${TEMPLATE_PATH_EXCEPT_EXT}.ts \
 && \
 npx @svgr/cli \
@@ -58,21 +60,78 @@ npx @svgr/cli \
 --out-dir ${COMPONENT_DIR} -- ${SVG_DIR} \
 && \
 rm ${TEMPLATE_PATH_EXCEPT_EXT}.js
-  `.trim());
+  `.trim(),
+  );
 
   console.log(`🚚 Creating [${TREE_FILENAME}] in [${COMPONENT_DIR}/] ...`);
-  const componentsTree = dirTree(COMPONENT_DIR, {
-    extensions: /\.tsx/,
-    attributes: ['type'],
-  }, ((item, path) => {
-    if (item.type !== 'file') return;
-  }));
+  const componentImportsMap: {
+    [dirPath: string]: {
+      name: string;
+      componentNames: string[];
+    };
+  } = {};
+
+  const componentsTree = dirTree(
+    COMPONENT_DIR,
+    {
+      extensions: /\.tsx/,
+      attributes: ['type'],
+    },
+    undefined,
+    (item, path) => {
+      const childFiles = item.children?.filter(child => child.type === 'file');
+      const hasChildFile = childFiles && childFiles.length !== 0;
+
+      if (!hasChildFile) return;
+
+      const isRootDir = item.name === COMPONENT_DIR;
+      componentImportsMap[path] = {
+        name: isRootDir ? 'root' : item.name,
+        componentNames: childFiles.map(file => file.name.replace('.tsx', '')),
+      };
+    },
+  );
   const componentsJsonTree = JSON.stringify(componentsTree, null, '\t');
-  await makeFile(`${COMPONENT_DIR}/${TREE_FILENAME}`, getTreeFileBody(componentsJsonTree));
+  await makeFile(
+    `${COMPONENT_DIR}/${TREE_FILENAME}`,
+    getTreeFileBody(componentsJsonTree),
+  );
 
   console.log(`🚚 Creating stories ...`);
+  const importsMapArr = Object.entries(componentImportsMap);
+
+  const fileBody =
+    `
+import React from 'react';
+import { getStoryBase, paletteFactory } from '@stories/StoryBase';
+${importsMapArr.reduce((acc, [dirPath, { componentNames }]) => {
+  const importedComponents = componentNames.join(', ');
+  const importLine = `import { ${importedComponents} } from '@${dirPath}'`;
+  return acc + importLine + '\r\n';
+}, '')}
+const base = getStoryBase();
+export default base;
+
+${importsMapArr.reduce((acc, [_, { name, componentNames }]) => {
+  const pascalCaseName = name
+    .replace(/[a-zA-Z0-9]+/g, w => {
+      return w[0].toUpperCase() + w.slice(1).toLowerCase();
+    })
+    .replace(/[-_]/, '');
+  const story = `
+export const ${pascalCaseName} = paletteFactory();
+${pascalCaseName}.args = {
+  icons: [${componentNames.join(', ')}],
+};
+  `.trim();
+
+  return acc + story + '\r\n\r\n';
+}, '')}
+  `.trim() + '\r\n';
+
+  await makeFile(`${STORY_DIR}/Icon.stories.tsx`, fileBody);
 };
 
-(async ()=> {
+(async () => {
   await generate();
 })();
--- svg/tree.ts
+++ svg/tree.ts
@@ -1,75 +1,34 @@
 // === made by querypie front-end team
-
 const tree = {
 	"path": "svg",
 	"name": "svg",
 	"children": [
 		{
-			"path": "svg/arrows",
-			"name": "arrows",
+			"path": "svg/a.svg",
+			"name": "a.svg",
+			"type": "file"
+		},
+		{
+			"path": "svg/one-depth",
+			"name": "one-depth",
 			"children": [
 				{
-					"path": "svg/arrows/ArrowDown.svg",
-					"name": "ArrowDown.svg",
+					"path": "svg/one-depth/b.svg",
+					"name": "b.svg",
 					"type": "file"
 				},
 				{
-					"path": "svg/arrows/ArrowDownSmall.svg",
-					"name": "ArrowDownSmall.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/arrows/ArrowLeft.svg",
-					"name": "ArrowLeft.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/arrows/ArrowRight.svg",
-					"name": "ArrowRight.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/arrows/ArrowUp.svg",
-					"name": "ArrowUp.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/arrows/ArrowUpSmall.svg",
-					"name": "ArrowUpSmall.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/arrows/twoDepths",
-					"name": "twoDepths",
+					"path": "svg/one-depth/two_depth",
+					"name": "two_depth",
 					"children": [
 						{
-							"path": "svg/arrows/twoDepths/ArrowDown.svg",
-							"name": "ArrowDown.svg",
+							"path": "svg/one-depth/two_depth/c.svg",
+							"name": "c.svg",
 							"type": "file"
 						},
 						{
-							"path": "svg/arrows/twoDepths/ArrowDownSmall.svg",
-							"name": "ArrowDownSmall.svg",
-							"type": "file"
-						},
-						{
-							"path": "svg/arrows/twoDepths/ArrowLeft.svg",
-							"name": "ArrowLeft.svg",
-							"type": "file"
-						},
-						{
-							"path": "svg/arrows/twoDepths/ArrowRight.svg",
-							"name": "ArrowRight.svg",
-							"type": "file"
-						},
-						{
-							"path": "svg/arrows/twoDepths/ArrowUp.svg",
-							"name": "ArrowUp.svg",
-							"type": "file"
-						},
-						{
-							"path": "svg/arrows/twoDepths/ArrowUpSmall.svg",
-							"name": "ArrowUpSmall.svg",
+							"path": "svg/one-depth/two_depth/d.svg",
+							"name": "d.svg",
 							"type": "file"
 						}
 					],
@@ -77,54 +36,6 @@ const tree = {
 				}
 			],
 			"type": "directory"
-		},
-		{
-			"path": "svg/base",
-			"name": "base",
-			"children": [
-				{
-					"path": "svg/base/ArrowDown.svg",
-					"name": "ArrowDown.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/base/ArrowDownSmall.svg",
-					"name": "ArrowDownSmall.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/base/ArrowLeft.svg",
-					"name": "ArrowLeft.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/base/ArrowRight.svg",
-					"name": "ArrowRight.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/base/ArrowUp.svg",
-					"name": "ArrowUp.svg",
-					"type": "file"
-				},
-				{
-					"path": "svg/base/ArrowUpSmall.svg",
-					"name": "ArrowUpSmall.svg",
-					"type": "file"
-				}
-			],
-			"type": "directory"
-		},
-		{
-			"path": "svg/temp",
-			"name": "temp",
-			"children": [],
-			"type": "directory"
-		},
-		{
-			"path": "svg/temp.svg",
-			"name": "temp.svg",
-			"type": "file"
 		}
 	],
 	"type": "directory"
