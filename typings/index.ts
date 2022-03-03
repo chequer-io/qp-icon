457fbeb1db0aac8228b916cb784205daf26b5709
Will Kim <will@chequer.io>
2022-03-04 01:42:47 +0900

    prettier

--- typings/index.ts
+++ typings/index.ts
@@ -8,40 +8,39 @@ type IconSize = IconSizeShort | IconSizeLong | 'responsive';
 
 type SizeAffectStyleProps = 'width' | 'height' | 'viewBox';
 interface CustomizedSvgProps {
-    /**
-     * @default: 'responsive'
-     *
-     * S_small: 16
-     *
-     * M_medium: 20
-     *
-     * L_large: 24
-     *
-     * XL_extraLarge: 32
-     *
-     * responsive: '1em'
-     */
-    size?: IconSize;
+  /**
+   * @default: 'responsive'
+   *
+   * S_small: 16
+   *
+   * M_medium: 20
+   *
+   * L_large: 24
+   *
+   * XL_extraLarge: 32
+   *
+   * responsive: '1em'
+   */
+  size?: IconSize;
 
-    /**
-     * @default: {}
-     */
-    style?: Omit<SVGProps<SVGSVGElement>['style'], SizeAffectStyleProps>;
+  /**
+   * @default: {}
+   */
+  style?: Omit<SVGProps<SVGSVGElement>['style'], SizeAffectStyleProps>;
 }
 
 type FilteredSvgElementProps = Omit<
-    SVGElementProps,
-    SizeAffectStyleProps | keyof CustomizedSvgProps
-    >;
+  SVGElementProps,
+  SizeAffectStyleProps | keyof CustomizedSvgProps
+>;
 
 export interface CustomizedSVGComponentProps
-    extends CustomizedSvgProps,
-        FilteredSvgElementProps {}
+  extends CustomizedSvgProps,
+    FilteredSvgElementProps {}
 export type CustomizedSVGComponent = FC<CustomizedSVGComponentProps>;
 
-export interface CommonSVGComponentProps
-    extends CustomizedSVGComponentProps {
-    viewBox: SVGProps<SVGSVGElement>['viewBox'];
+export interface CommonSVGComponentProps extends CustomizedSVGComponentProps {
+  viewBox: SVGProps<SVGSVGElement>['viewBox'];
 }
 export type CommonSVGComponent = FC<CommonSVGComponentProps>;
 
