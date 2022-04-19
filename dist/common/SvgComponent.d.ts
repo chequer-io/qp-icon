1551c3c77719bea7ec78a3972a538a115fecaedb
Will Kim <will@chequer.io>
2022-04-19 18:02:57 +0900

    0.0.4

--- dist/common/SvgComponent.d.ts
+++ dist/common/SvgComponent.d.ts
@@ -1,8 +1,4 @@
-import { CommonSVGComponent, IconSize, NotUndefined, SVGElementProps } from '../typings';
-declare type SVGWidthOrHeight = NotUndefined<SVGElementProps['width'] | SVGElementProps['height']>;
-export declare const sizeMap: {
-    [key in NotUndefined<IconSize>]: SVGWidthOrHeight;
-};
+import { CommonSVGComponent } from '../typings';
 declare const SvgComponent: CommonSVGComponent;
 export default SvgComponent;
 //# sourceMappingURL=SvgComponent.d.ts.map
