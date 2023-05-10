d4874e4eefb55884669468973ebacc1feab4a422
Will Kim <will@chequer.io>
2023-05-10 15:29:12 +0900

    0.0.12

--- src/components/File/QIFileAdd.tsx
+++ src/components/File/QIFileAdd.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import SvgComponent from '../../common/SvgComponent';
 import { CustomizedSVGComponent } from '../../typings';
 
-export const QIServerSingle: CustomizedSVGComponent = ({ ...props }) => (
+export const QIFileAdd: CustomizedSVGComponent = ({ ...props }) => (
   <SvgComponent
     viewBox="0 0 32 32"
     role="qp-icon"
@@ -12,7 +12,7 @@ export const QIServerSingle: CustomizedSVGComponent = ({ ...props }) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M4 21H28C29.1 21 30 20.1 30 19V13C30 11.9 29.1 11 28 11H4C2.9 11 2 11.9 2 13V19C2 20.1 2.9 21 4 21ZM4 19V13H28V19H4ZM6 16C6 15.4477 6.44772 15 7 15V15C7.55228 15 8 15.4477 8 16V16C8 16.5523 7.55228 17 7 17V17C6.44772 17 6 16.5523 6 16V16Z"
+      d="M6 28H14V30H6C4.90002 30 4 29.1 4 28V4C4 2.90002 4.90002 2 6 2H16C16.3 2 16.5 2.09998 16.7 2.30005L23.7 9.30005C23.9 9.5 24 9.69995 24 10V16H22V12H16C14.9 12 14 11.1 14 10V4H6V28ZM21.6 10L16 4.40002V10H21.6ZM24 24H28V26H24V30H22V26H18V24H22V20H24V24Z"
       fill="#656C72"
     />
   </SvgComponent>
